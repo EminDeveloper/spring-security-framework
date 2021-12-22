a369ee46550362d3974c6b676a7de7a998a829c9
Emin Developer <emin.f.mammadov@gmail.com>
2021-12-21 16:35:06 -0800

    Extending WebSecurityConfigurerAdapter

--- HelloSpringSecurity/src/main/java/com/london/hellospringsecurity/config/ProjectConfig.java
+++ HelloSpringSecurity/src/main/java/com/london/hellospringsecurity/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.london.hellospringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsService =
