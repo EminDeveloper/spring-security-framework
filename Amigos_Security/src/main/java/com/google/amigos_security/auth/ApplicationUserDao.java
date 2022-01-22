59a11075ec4a9d506a060d438eb3b6d908623956
Emin <emin.f.mammadov@gmail.com>
2022-01-22 12:30:35 +0400

    APPLICATION USER DAO INTERFACE

---
+++ Amigos_Security/src/main/java/com/google/amigos_security/auth/ApplicationUserDao.java
@@ -0,0 +1,8 @@
+package com.google.amigos_security.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
